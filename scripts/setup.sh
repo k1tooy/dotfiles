a13ce91243bcad92ad781e4d1deff1775c95b953
k1tooy <108557464+k1tooy@users.noreply.github.com>
2024-08-23 20:02:36 +0800

    edit setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,18 +8,11 @@ HOME_DIR=$HOME
 
 # Install packages
 install_packages() {
-  packages=("zsh" "kitty" "git" "neovim" "npm" "python" "gcc" "zoxide" "bat" "fzf" "ripgrep" "eza" "tmux" "poetry")
+  packages=("zsh" "kitty" "git" "neovim" "npm" "python" "gcc" "zoxide" "bat" "fzf" "ripgrep" "eza" "tmux" "poetry" "fd")
   echo "Installing packages...."
   sudo pacman -Syu
   sudo pacman -S "${packages[@]}"
 }
 
-# Copy config folder
-setup_config() {
-  stow init
-}
-
 install_packages
-setup_config
-
-echo "Developer Setup Complete!"
+echo "Packages have been installed!"
