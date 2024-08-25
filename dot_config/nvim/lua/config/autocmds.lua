35dc3079303f0b4a06ff000defa993166f3c1737
k1tooy <108557464+k1tooy@users.noreply.github.com>
2024-08-25 12:44:51 +0800

    restore tmux statusline on quit

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Disable Zen Mode and restore tmux statusline on quit
+vim.api.nvim_create_autocmd("QuitPre", {
+  pattern = "*",
+  callback = function()
+    vim.cmd("silent! ZenMode!")
+    vim.fn.system("tmux set status on")
+  end,
+})
