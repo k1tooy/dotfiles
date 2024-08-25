84504a20b2816fad315852d9666753c3938508d0
k1tooy <108557464+k1tooy@users.noreply.github.com>
2024-08-25 09:27:59 +0800

    add mason config

---
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,13 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "bash-language-server",
+      "clangd",
+      "css-lsp",
+      "pyright",
+      "mypy",
+      "ruff",
+    },
+  },
+}
