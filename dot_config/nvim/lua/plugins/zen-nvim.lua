4e26c20b7c8133cff5a3704261fd54a1d54f52a9
k1tooy <108557464+k1tooy@users.noreply.github.com>
2024-08-25 18:58:43 +0800

    add zen mode hotkey

--- dot_config/nvim/lua/plugins/zen-nvim.lua
+++ dot_config/nvim/lua/plugins/zen-nvim.lua
@@ -2,14 +2,17 @@ return {
   "folke/zen-mode.nvim",
   opts = {
     window = {
-      backdrop = 0.90,
-      height = 0.8,
-      width = 100,
+      backdrop = 0.80,
+      height = 0.70,
+      width = 90,
       options = { signcolumn = "no", number = true, cursorline = false },
     },
     plugins = {
       options = { enabled = true, ruler = false, showcmd = false, laststatus = 1 },
       tmux = { enabled = true },
     },
+  },
+  keys = {
+    { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" },
   },
 }
