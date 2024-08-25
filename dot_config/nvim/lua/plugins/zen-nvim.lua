fb92456a79391cb85b984d68e4fe6e9a1c90a7af
k1tooy <108557464+k1tooy@users.noreply.github.com>
2024-08-25 09:28:26 +0800

    add zen-nvim

--- dot_config/nvim/lua/plugins/zen-nvim.lua
+++ dot_config/nvim/lua/plugins/zen-nvim.lua
@@ -1,8 +1,15 @@
 return {
   "folke/zen-mode.nvim",
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    window = {
+      backdrop = 0.90,
+      height = 0.8,
+      width = 100,
+      options = { signcolumn = "no", number = true, cursorline = false },
+    },
+    plugins = {
+      options = { enabled = true, ruler = false, showcmd = false, laststatus = 1 },
+      tmux = { enabled = true },
+    },
   },
 }
